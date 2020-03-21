147ec393e47ebc4aa826e22e56bf2aa2b958b3d9
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2020-03-21 18:28:20 +0100

    Modified build script and kernel to create hybrid compatible modules with esm and commonjs

--- decorators/View.ts
+++ decorators/View.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import * as path from "path";
 import {BaseEvent} from "../BaseEvent";
 import {Response} from "../http/Response";
 import {JSONResponse} from "../http/JSONResponse";
