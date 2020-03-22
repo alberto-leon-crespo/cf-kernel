fa2888dac8522e8086f93fdc470a4d5ebd8a17a0
Alberto León Crespo <alberto.leon.crespo@gmail.com>
2020-03-22 03:31:58 +0100

    Fixed problems

--- decorators/Controller.ts
+++ decorators/Controller.ts
@@ -1,9 +1,18 @@
 import { BaseController } from "../BaseController";
 
-export const Controller = <T extends BaseController>(constructor: any): T => {
-    return new(class extends BaseController {
+export const Controller = <T extends BaseController>(constructor: Function) => {
+    let originalController = new constructor();
+    return class extends BaseController {
         constructor() {
             super();
+            const methods = Object.getOwnPropertyNames(constructor.prototype);
+            const properties = Object.getOwnPropertyNames(originalController);
+            for (const methodName of methods) {
+                this[methodName] = originalController[methodName];
+            }
+            for (const propertyName of properties) {
+                this[propertyName] = originalController[propertyName];
+            }
         }
-    })() as T;
+    };
 };
